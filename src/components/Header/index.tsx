0210730a371b4e906271b6eb71a13389f3e07341
joenog <joabnogueira@outlook.com>
2025-04-22 18:37:46 -0300

    types and api added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,19 +3,20 @@ import { Link } from "react-router-dom";
 
 export function Header() {
   return (
-    <header className="flex justify-center bg-neutral-900 h-16 rounded-xl m-2 text-amber-50">
-
+    <header className="flex justify-center bg-neutral-900/90 h-16 rounded-xl m-2 text-amber-50">
       <nav className="flex w-full max-w-7xl pr-4 items-center justify-between">
         <Link to={"/"}>
-          <h1 className="text-2xl font-bold pl-4"><span className="text-green-300">super</span>CART</h1>
+          <h1 className="text-2xl font-bold pl-4">
+            <span className="text-green-300">super</span>CART
+          </h1>
         </Link>
-          <Link to={"/cart"}>
-            {" "}
-            <FaCartShopping size={20}/>{" "}
-            <span className="absolute bg-red-500 px-1 rounded-2xl text-xs">
-              {'3'}
-            </span>
-          </Link>
+        <Link to={"/cart"}>
+          {" "}
+          <FaCartShopping size={20} />{" "}
+          <span className="absolute bg-red-500 px-1 rounded-2xl text-xs">
+            {"3"}
+          </span>
+        </Link>
       </nav>
     </header>
   );
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,42 +1,43 @@
+import { useState, useEffect } from "react";
+import { api } from "../../services/api";
+import { ProductsProps } from "../../types/productsProps";
 import { FaCartShopping } from "react-icons/fa6";
 
 export default function Home() {
+  const [products, setProducts] = useState<ProductsProps[]>([]);
+
+  useEffect(() => {
+    async function getProducts() {
+      const response = await api.get("/products");
+      setProducts(response.data)
+    }
+    getProducts();
+  }, []);
+
   return (
     <div>
-      <main className="flex justify-center flex-col w-full max-w-5xl mx-auto">
-          <h1 className=" text-xl my-5 text-center">Produtos em alta</h1>
+      <main className="flex justify-center flex-col w-full max-w-5xl mx-auto mb-17">
+        <h1 className=" text-xl my-5 text-center">Produtos em alta</h1>
         <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 rounded-xl gap-4">
-         <section className="flex flex-col bg-white rounded-2xl p-4">
-              <img className="w-full max-h-70 mb-2" src="https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcT8RfGyD89MqNeiviyMiC2cf30r17rMSS6vcgmkijdarPCN8ITQIrX4IzqZecyiGBCgJh4NGOLzt1iX9_BzWG0h0AsXRWnx49LfWTesfpteMfg3nzVfnf0yLw" alt="" />
-            
-                <p className="mt-6">
-                  Fones JBl ultraboost
-                </p>
-              <div className="flex h-full items-end gap-4 mt-2">
-                <span className="text-zinc-900/90">
-                  R$ 190,20
-                </span>
-                <span className="bg-green-500/80 rounded-md p-1">
-                  <FaCartShopping size={17}/>
-                </span>
-              </div>
-            </section>
 
-            <section className="flex flex-col bg-white rounded-2xl p-4">
-              <img className="w-full mx-h-70 mb-2" src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSfG_55OjJtxhhGUUHZfSIFCYjSGr3Uo4lnasIYsOhIoLICnwIAvJ8JFBtXv3QiGph7w9tQYFCAu0cMsXV88QZnJF2xEna_TPAEaMJGlg6VNrV-tsKnas-6" alt="" />
-            
-                <p className="mt-6">
-                  Fones JBl ultraboost
-                </p>
-              <div className="flex h-full items-end gap-4 mt-2">
-                <span className="text-zinc-900/90">
-                  R$ 190,20
-                </span>
-                <span className="bg-green-500/80 rounded-md p-1">
-                  <FaCartShopping size={17}/>
-                </span>
+          {products.map((product)=> (
+            <section className="flex flex-col bg-white rounded-2xl p-4" key={product.id}>
+              <div className="mb-3">
+                <img src={product.cover} alt=""></img>
+                {product.title}
+              </div>
+              <div className="flex h-full font-bold justify-between items-end">
+                {product.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL"
+                })}
+
+                <button className="bg-green-400 px-4 p-1 rounded-md">
+                  <FaCartShopping size={15} />
+                </button>
               </div>
             </section>
+          ))}
 
         </div>
       </main>
